b5b94a2c7e4a214239271b0ceccfd4398580f919
Marek Fajkus <marek.faj@gmail.com>
2017-03-21 19:09:38 +0100

    Simple mutable Queue implementation

---
+++ spec/utils/queue_spec.js
@@ -0,0 +1,99 @@
+var Queue = require('../../src/utils/queue');
+
+describe('Queue', function() {
+    describe('#enque / #dequeue', function() {
+        var queue = Queue();
+
+        it('Should enqueue 1', function() {
+            queue.enqueue(1);
+            expect(queue.size()).toEqual(1);
+        });
+
+        it('Should keep a state', function() {
+            queue.enqueue(2);
+            expect(queue.size()).toEqual(2);
+        });
+
+        it('should deque with resukt', function() {
+            var result = queue.dequeue();
+            expect(result.value).toEqual(1);
+            expect(result.next).toEqual(queue);
+            expect(queue.size()).toEqual(1);
+        });
+
+        it('should deque to empty', function() {
+            var result = queue.dequeue();
+            expect(result.value).toEqual(2);
+            expect(result.next).toEqual(null);
+            expect(queue.size()).toEqual(0);
+        });
+    });
+
+    describe('#subscribe', function() {
+        var queue = Queue();
+        var result;
+
+        describe('simple case', function() {
+            beforeEach(function(done) {
+                queue.subscribe(function(res) {
+                    result = res;
+                    done();
+                });
+
+                queue.enqueue(0);
+            });
+
+            it('should call subscriber', function() {
+                var dequeue = result.dequeue();
+                expect(dequeue.value).toEqual(0);
+                expect(dequeue.next).toEqual(null);
+            });
+        });
+
+        describe('subscribe to enqued queue', function() {
+            beforeEach(function(done) {
+                queue.enqueue(0);
+
+                queue.subscribe(function(res) {
+                    result = res;
+                    done();
+                });
+
+                queue.enqueue(1);
+            });
+
+            it('should call subscriber', function() {
+                var dequeue = result.dequeue();
+                expect(dequeue.value).toEqual(0);
+                expect(dequeue.next).toEqual(queue);
+                expect(queue.size()).toEqual(1);
+
+                var dequeue2 = dequeue.next.dequeue();
+                expect(dequeue2.value).toEqual(1)
+                expect(dequeue2.next).toEqual(null);
+                expect(queue.size()).toEqual(0);
+            });
+        });
+    });
+
+    describe('#unSubscribe', function() {
+        var queue = Queue();
+
+        it('should not call calback after unsubscription', function(done) {
+            var result;
+
+            function callback(res) {
+                result = res;
+            }
+
+            queue.subscribe(callback);
+            queue.unSubscribe(callback);
+            queue.enqueue(1);
+
+            setTimeout(function() {
+                expect(result).toEqual(undefined);
+                done();
+            }, 10);
+        });
+    });
+});
---
+++ src/utils/queue.js
@@ -0,0 +1,50 @@
+// Mutable Queue implementation with subscriber
+// Please be aware of fact that even though this implementation supports
+// multiple subscribers `dequeue` method is actually mutating state
+// therefore calling `dequeue` in multiple subscribers causes that race condition in state!
+
+function Queue() {
+    // initial state
+    var state = [];
+    var callbacks = [];
+
+    var publicMethods = {
+        subscribe: function(callback) {
+            if (typeof callback !== 'function') {
+                throw('Subcribe requires function');
+            }
+            callbacks.push(callback);
+
+            return publicMethods;
+        },
+        unSubscribe: function(callback) {
+            var index = callbacks.indexOf(callback);
+            callbacks.splice(index, 1);
+
+            return publicMethods;
+        },
+        enqueue: function(item) {
+            state.push(item);
+
+            // call subscribers
+            callbacks.forEach(function(cb) {
+                cb(publicMethods);
+            });
+
+            return publicMethods;
+        },
+        dequeue: function() {
+            return {
+                value: state.shift(),
+                next: state.length > 0 ? publicMethods : null
+            }
+        },
+        size: function() {
+            return state.length;
+        }
+    }
+
+    return publicMethods;
+}
+
+module.exports = Queue;
