5c31d709a21d8cad88422c669deedebf57cda1b6
Marek Fajkus <marek.faj@gmail.com>
2017-03-24 20:34:52 +0100

    make diff understantable

--- src/segment.js
+++ src/segment.js
@@ -9,109 +9,106 @@ var library = {
 }
 
 // constructor
-function Segment(key, context, btoa){
-  var userId = 'anonymous';
-  var queue = Queue();
-  btoa = btoa || window.btoa;
-
-  // meta
-  context = context || {};
-  context.library = context.library || {};
-  context.library.name = library.name;
-  context.library.version = library.version;
-
-  // api settings
-  var baseUrl = 'https://api.segment.io/v1/';
-  var method = 'POST';
-  var headers = {
-    'Authorization': 'Basic ' + btoa(key + ':'),
-    'Content-Type': 'application/json'
-  };
-
-  // api request
-  function apiCall(type, body) {
-    body['context'] = context;
-
-    return fetch(
-      baseUrl + type,
-      {
-        method: method,
-        headers: headers,
-        body: JSON.stringify(body)
-      }
-    );
-  };
-
-  function lazyApiCall(type, body) {
-    return new Promise(function(resolve) {
-      queue.enqueue(function() {
-        apiCall(type, body).then(function(result) {
-          resolve(result);
-        })
-      })
-    });
-  }
-
-  // user tracks
-  queue.subscribe(function(queue) {
-    if (userId === 'annonymous') return;
-
-    while(queue) {
-      var lastEvent = queue.dequeue();
-      lastEvent.value();
-      queue = lastEvent.next;
-    }
-  });
-
-  // public interface
-  return {
-    identify: function(id, traits){
-      userId = id;
-
-      return apiCall('identify', {
-        userId: id,
-        traits: traits
-      });
-    },
-
-    track: function(event, properties){
-      return lazyApiCall('track', {
-        userId: userId,
-        event: event,
-        properties: properties
-      });
-    },
-
-    anonymousTrack: function(anonymousId, event, properties){
-      return apiCall('track', {
-        anonymousId: anonymousId,
-        event: event,
-        properties: properties
-      });
-    },
-
-    page: function(name, properties) {
-      return lazyApiCall('page', {
-        userId: userId,
-        name: name,
-        properties: properties
-      });
-    },
-
-    anonymousPage: function(anonymousId, name, properties) {
-      return apiCall('page', {
-        anonymousId: anonymousId,
-        name: name,
-        properties: properties
-      });
-    },
-
-    version: library.version,
-    name: library.name
-  }
-}
-
-// export module functions
 module.exports = {
-  getClient: Segment
+  getClient: function(key, context, btoa) {
+    var userId = 'anonymous';
+    var queue = Queue();
+    btoa = btoa || window.btoa;
+
+    // meta
+    context = context || {};
+    context.library = context.library || {};
+    context.library.name = library.name;
+    context.library.version = library.version;
+
+    // api settings
+    var baseUrl = 'https://api.segment.io/v1/';
+    var method = 'POST';
+    var headers = {
+      'Authorization': 'Basic ' + btoa(key + ':'),
+      'Content-Type': 'application/json'
+    };
+
+    // api request
+    function apiCall(type, body) {
+      body['context'] = context;
+
+      return fetch(
+        baseUrl + type,
+        {
+          method: method,
+          headers: headers,
+          body: JSON.stringify(body)
+        }
+      );
+    };
+
+    function lazyApiCall(type, body) {
+      return new Promise(function(resolve) {
+        queue.enqueue(function() {
+          apiCall(type, body).then(function(result) {
+            resolve(result);
+          })
+        })
+      });
+    }
+
+    // user tracks
+    queue.subscribe(function(queue) {
+      if (userId === 'annonymous') return;
+
+      while(queue) {
+        var lastEvent = queue.dequeue();
+        lastEvent.value();
+        queue = lastEvent.next;
+      }
+    });
+
+    // public interface
+    return {
+      identify: function(id, traits){
+        userId = id;
+
+        return apiCall('identify', {
+          userId: id,
+          traits: traits
+        });
+      },
+
+      track: function(event, properties){
+        return lazyApiCall('track', {
+          userId: userId,
+          event: event,
+          properties: properties
+        });
+      },
+
+      anonymousTrack: function(anonymousId, event, properties){
+        return apiCall('track', {
+          anonymousId: anonymousId,
+          event: event,
+          properties: properties
+        });
+      },
+
+      page: function(name, properties) {
+        return lazyApiCall('page', {
+          userId: userId,
+          name: name,
+          properties: properties
+        });
+      },
+
+      anonymousPage: function(anonymousId, name, properties) {
+        return apiCall('page', {
+          anonymousId: anonymousId,
+          name: name,
+          properties: properties
+        });
+      },
+
+      version: library.version,
+      name: library.name
+    }
+  }
 };
