111a53926743efac89d6c42c627f16cbb1731e5e
Marek Fajkus <marek.faj@gmail.com>
2017-03-27 19:34:50 +0200

    Test mock

--- src/segment.js
+++ src/segment.js
@@ -8,106 +8,146 @@ var library = {
   version: '2.0.0-alpha1'
 }
 
-// constructor
-module.exports = {
-  getClient: function(key, context, btoa) {
-    var userId = 'anonymous';
-    var queue = Queue();
-    btoa = btoa || window.btoa;
+function constructAdapter(mockQueue, key, btoa) {
+  // api settings
+  btoa = btoa || window.btoa;
+  var baseUrl = 'https://api.segment.io/v1/';
+  var method = 'POST';
+  var headers = {
+    'Authorization': 'Basic ' + btoa(key + ':'),
+    'Content-Type': 'application/json'
+  };
 
-    // meta
-    context = context || {};
-    context.library = context.library || {};
-    context.library.name = library.name;
-    context.library.version = library.version;
+  return function(type, body) {
+    // test mock adapter
+    if (mockQueue) {
+      return new Promise(function(resolve) {
+        setTimeout(function() {
+          mockQueue.enqueue({
+            type: type,
+            body: body
+          });
 
-    // api settings
-    var baseUrl = 'https://api.segment.io/v1/';
-    var method = 'POST';
-    var headers = {
-      'Authorization': 'Basic ' + btoa(key + ':'),
-      'Content-Type': 'application/json'
-    };
-
-    // api request
-    function apiCall(type, body) {
-      body.context = context;
-
-      return fetch(
-        baseUrl + type,
-        {
-          method: method,
-          headers: headers,
-          body: JSON.stringify(body)
-        }
-      );
-    }
-
-    function lazyApiCall(type, body) {
-      return new Promise(function(resolve, reject) {
-        queue.enqueue(function() {
-          body.userId = userId; // set userId
-
-          apiCall(type, body)
-            .then(resolve)
-            .catch(reject);
-        });
+          resolve({ status: 200 });
+        }, 0);
       });
     }
 
-    // user tracks
-    queue.onEnqueue(function(queue) {
-      if (userId === 'anonymous') return;
-
-      while(queue) {
-        var lastEvent = queue.dequeue();
-        lastEvent.value();
-        queue = lastEvent.next;
+    // reqular segment adapter
+    return fetch(
+      baseUrl + type,
+      {
+        method: method,
+        headers: headers,
+        body: JSON.stringify(body)
       }
+    );
+  }
+}
+
+function Constructor(adapter, context) {
+  var userId = 'anonymous';
+  var queue = Queue();
+
+  // meta
+  context = context || {};
+  context.library = context.library || {};
+  context.library.name = library.name;
+  context.library.version = library.version;
+
+  function apiCall(type, body) {
+    body.context = context;
+    return adapter(type, body);
+  }
+
+  function lazyApiCall(type, body) {
+    return new Promise(function(resolve, reject) {
+      queue.enqueue(function() {
+        body.userId = userId; // set userId
+
+        apiCall(type, body)
+          .then(resolve)
+          .catch(reject);
+      });
     });
+  }
 
-    // public interface
-    return {
-      identify: function(id, traits) {
-        userId = id;
+  // user tracks
+  queue.onEnqueue(function(queue) {
+    if (userId === 'anonymous') return;
 
-        return lazyApiCall('identify', {
-          traits: traits
-        });
+    while(queue) {
+      var lastEvent = queue.dequeue();
+      lastEvent.value();
+      queue = lastEvent.next;
+    }
+  });
+
+  // public interface
+  return {
+    identify: function(id, traits) {
+      userId = id;
+
+      return lazyApiCall('identify', {
+        traits: traits
+      });
+    },
+
+    track: function(event, properties) {
+      return lazyApiCall('track', {
+        event: event,
+        properties: properties
+      });
+    },
+
+    anonymousTrack: function(anonymousId, event, properties) {
+      return apiCall('track', {
+        anonymousId: anonymousId,
+        event: event,
+        properties: properties
+      });
+    },
+
+    page: function(name, properties) {
+      return lazyApiCall('page', {
+        name: name,
+        properties: properties
+      });
+    },
+
+    anonymousPage: function(anonymousId, name, properties) {
+      return apiCall('page', {
+        anonymousId: anonymousId,
+        name: name,
+        properties: properties
+      });
+    },
+
+    version: library.version,
+    name: library.name
+  }
+}
+
+// constructor
+module.exports = {
+  getClient: function(key, context, btoa) {
+    return Constructor(constructAdapter(false, key, btoa), context);
+  },
+  getTestMockClient: function(key, context, btoa) {
+    mockQueue = Queue();
+
+    var publicApi = Constructor(constructAdapter(mockQueue, key, btoa), context);
+
+    // mock inspect API
+    publicApi.inspect = {
+      allTracks: function() {
+        return mockQueue.toArray();
       },
 
-      track: function(event, properties) {
-        return lazyApiCall('track', {
-          event: event,
-          properties: properties
-        });
-      },
-
-      anonymousTrack: function(anonymousId, event, properties) {
-        return apiCall('track', {
-          anonymousId: anonymousId,
-          event: event,
-          properties: properties
-        });
-      },
-
-      page: function(name, properties) {
-        return lazyApiCall('page', {
-          name: name,
-          properties: properties
-        });
-      },
-
-      anonymousPage: function(anonymousId, name, properties) {
-        return apiCall('page', {
-          anonymousId: anonymousId,
-          name: name,
-          properties: properties
-        });
-      },
-
-      version: library.version,
-      name: library.name
+      lastTrack: function() {
+        var array = mockQueue();
+        return array[array.length - 1];
+      }
     }
   }
 };
