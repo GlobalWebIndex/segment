de9e734414e38d9cab0735589236349eef6179d4
Marek Fajkus <marek.faj@gmail.com>
2017-03-29 16:31:43 +0200

    add toArray test

--- spec/utils/queue_spec.js
+++ spec/utils/queue_spec.js
@@ -83,8 +83,6 @@ describe('Queue', function() {
     });
 
     describe('unsubscribe', function() {
-      var queue = Queue();
-
       it('should not call calback after unsubscription', function(done) {
         var result;
 
@@ -102,6 +100,16 @@ describe('Queue', function() {
           done();
         }, 10);
       });
+    });
+  });
+
+  describe('#toArray', function() {
+    it('should return array', function() {
+      queue.enqueue(1);
+      queue.enqueue(2);
+      queue.enqueue(3);
+
+      expect(queue.toArray()).toEqual([1,2,3]);
     });
   });
 });
